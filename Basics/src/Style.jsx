5d6c50bf94d352e0df7ec35d0584766667f96cc6
prafulpawar <prafuldukhi@gmail.com>
2025-05-23 17:14:43 +0530

    stylesheetAdded

--- Basics/src/Style.jsx
+++ Basics/src/Style.jsx
@@ -1,48 +1,61 @@
-import { View, Text ,Image, SafeAreaView,StyleSheet, Pressable } from 'react-native'
+import { View, Text, Image, SafeAreaView, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 const Style = () => {
+    return (
 
-  return (
+        <SafeAreaView style={styles.container}>
+
+            <Text style={styles.text}>Hello </Text>
+            <Image source={{ uri: 'https://i.natgeofe.com/k/679e983c-4461-4398-bb6d-9b508fe3e4de/norway-northern-lights_2x3.jpg' }}
+                style={{ width: 200, height: 200 }} />
+
+            <Pressable style={styles.button}>
+                <Text style={styles.btnText}
+                    onPress={() => Alert.alert('Hello Dost')}
+                    onHover={() => Alert.alert('Hello Dost')}
+                >
+                    Press Me
+                </Text>
+            </Pressable>
+
+        </SafeAreaView>
 
 
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Style</Text>
-        <Image source={{ uri: 'https://i.natgeofe.com/k/679e983c-4461-4398-bb6d-9b508fe3e4de/norway-northern-lights_2x3.jpg' }}
-                      style={{ width: 200, height: 200 }} />       
-   
-        <Pressable>
-            <Text style={{ padding: 10, backgroundColor: 'blue' }}
-                onPress={() => Alert.alert('Hello Dost')}
-                onHover={() => Alert.alert('Hello Dost')}
-            >
-                Bhai Mere Chotu
-            </Text>
-        </Pressable>
-    </SafeAreaView>
-
-
-  )
+    )
 }
 
 // In React Native, we use camelCase for styles (e.g., backgroundColor, fontSize).
 // This is different from CSS where we use kebab-case (e.g., background-color, font-size).
 
 const styles = StyleSheet.create({
-    container:{
-        with:100,
-        height:100,
-        backgroundColor:'#ffff',
-        borderColor:'red',
-        borderWidth:2,
+    container: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#ffffff',
+        borderWidth: 2,
+        padding:20,
+        gap:10
     },
-    text:{
-        color:'red',
-        fontSize:20,
-        fontWeight:'bold',
-        textAlign:'center',
-        marginTop:10,
-        padding:10,
-        backgroundColor:'blue'
+
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    button:{
+         width:100,
+         padding:10,
+         backgroundColor:"#666",
+         borderRadius:50,
+         alignItems:'center',
+         marginTop:10,
+         borderWidth:2,
+         borderColor:'red'
+    },
+    btnText:{
+        color: 'white',
+        fontWeight: '500',
+        
     }
 })
 
