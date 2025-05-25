d1a3255640a248012e2121909a5844368912e3f4
prafulpawar <prafuldukhi@gmail.com>
2025-05-25 16:17:49 +0530

    scrollAdded

--- Basics/App.jsx
+++ Basics/App.jsx
@@ -1,17 +1,19 @@
 import { View, Text, Image ,Button, TouchableOpacity, Alert, TouchableHighlight, Pressable, SafeAreaView } from 'react-native';
-import Basics from './src/Basics';
-import Style from './src/Style';
-import ThemeBased from './src/ThemeBased';
-import Units from './src/Units';
-import Flex from './src/Flex';
-import FlexDirection from './src/FlexDirection';
+import Basics from './src/Basics/Basics';
+import Style from './src/Basics/Style';
+import ThemeBased from './src/Basics/ThemeBased';
+import Units from './src/Basics/Units';
+import Flex from './src/Basics/Flex';
+import FlexDirection from './src/Basics/FlexDirection';
+import Scroll from './src/Basics/Scroll';
 
 
 const App = () => {
   return (
         //  <Units/>  
         // <Flex/>
-        <FlexDirection/>
+        // <FlexDirection/>
+        <Scroll/>
   )
 }
 
