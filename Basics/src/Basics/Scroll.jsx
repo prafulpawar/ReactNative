b6d0456654d9b11fe6ba5385b9f38dd13ccf9ec0
prafulpawar <prafuldukhi@gmail.com>
2025-05-25 16:29:54 +0530

    StyleBox

--- Basics/src/Basics/Scroll.jsx
+++ Basics/src/Basics/Scroll.jsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Scroll = () => {
-  return (
-    <View>
-      <Text>Scroll</Text>
-    </View>
-  )
+    return (
+        <View style={Style.container}>
+            <Text style={Style.box1} >Scroll</Text>
+            <Text style={Style.box2} >Scroll</Text>
+            <Text style={Style.box3} >Scroll</Text>
+        </View>
+    )
 }
 
 export default Scroll
+
+
+const Style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    box1: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+    },
+
+    box2: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'green',
+    },
+
+    box3: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'blue',
+    },
+})
