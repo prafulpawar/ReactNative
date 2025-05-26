d01adc559d5f9c58e95833829269a5a74a12ac99
prafulpawar <prafuldukhi@gmail.com>
2025-05-26 14:20:15 +0530

    basicFormDone

--- Basics/src/UserInput/Input.jsx
+++ Basics/src/UserInput/Input.jsx
@@ -1,32 +1,47 @@
-import { View, Text, StyleSheet ,TextInput ,Button } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 import React, { useState } from 'react'
 
 const Input = () => {
- const [Input , SetInput] = useState('');
-  
+  const [Input, SetInput] = useState('');
+  const [SubmittdText, setSubmittedText] = useState('')
+
+  const handleSubmit = () => {
+    setSubmittedText(Input)
+    SetInput('')
+  }
+
   return (
     <View style={Style.container}>
       <Text style={Style.title}>
-        
-        Hey How Are You
 
+        Hey How Are You
 
       </Text>
 
       <TextInput style={Style.input}
         placeholder='Enter An Text Here...'
         value={Input}
-        onChangeText={(e)=>SetInput(e.target.value)}
+        onChangeText={SetInput}
+
+        //  More ProPerties
+        //  multiline
+        //  numberOfLines={4}
+        //  keyboardType='numeric'
+        
       />
 
-      <Button title='Submit'>
-          
+      <Button title='Submit' onPress={handleSubmit} >
+
       </Button>
 
 
+      {
+        SubmittdText ? (
         <Text>
-              Result:  {Input}
-        </Text>
+          Result:  {SubmittdText}
+        </Text>) : null
+      }
+
     </View>
   )
 }
@@ -34,22 +49,22 @@ const Input = () => {
 export default Input
 
 const Style = StyleSheet.create({
-     container:{
-          flex:1,
-          justifyContent:'center',
-          alignItems:'center',
-          padding:20,
-          gap:10
-     },
-     title:{
-         fontSize:20,
-         fontWeight:'500'
-     },
-     input:{
-         width:'100%',
-         padding:5,
-         borderWidth:2,
-         borderRadius:5,
-         paddingVertical:10
-     }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    gap: 10
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '500'
+  },
+  input: {
+    width: '100%',
+    padding: 5,
+    borderWidth: 2,
+    borderRadius: 5,
+    paddingVertical: 10
+  }
 })
